a7ecfe104dfbe152bdcf80637a5e104eb54b84fe
Alexander Wolf <alex.v.wolf@gmail.com>
2015-08-28 20:38:58 +0600

    Added computation of aberration

---
+++ src/core/planetsephems/aberration.c
@@ -0,0 +1,254 @@
+/*
+Copyright (C) 2015 Alexander Wolf
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335, USA.
+*/
+
+#include <math.h>
+
+#define TERMS 36
+
+// Data structures to hold arguments and coefficients of Ron-Vondrak theory
+struct arg
+{
+	double L2;
+	double L3;
+	double L4;
+	double L5;
+	double L6;
+	double L7;
+	double L8;
+	double LL;
+	double D;
+	double MM;
+	double F;
+};
+
+struct XYZ
+{
+	double sin1;
+	double sin2;
+	double cos1;
+	double cos2;
+};
+
+// Table 23.A; argument column (p. 154-155)
+static const struct arg arguments[TERMS] =
+{
+      //L2  L3  L4  L5  L6  L7  L8  L'   D  M'   F
+	{0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0},
+	{0,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1},
+	{0,  0,  0,  0,  0,  0,  0,  1,  0,  1,  0},
+	{0,  0,  0,  2,  0,  0,  0,  0,  0,  0,  0},
+	{0,  2,  0, -1,  0,  0,  0,  0,  0,  0,  0},
+	{0,  3, -8,  3,  0,  0,  0,  0,  0,  0,  0},
+	{0,  5, -8,  3,  0,  0,  0,  0,  0,  0,  0},
+	{2, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0},
+	{0,  1,  0, -2,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0},
+	{0,  1,  0,  1,  0,  0,  0,  0,  0,  0,  0},
+	{2, -2,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  1,  0, -1,  0,  0,  0,  0,  0,  0,  0},
+	{0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  3,  0, -2,  0,  0,  0,  0,  0,  0,  0},
+	{1, -2,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{2, -3,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  2,  0,  0,  0,  0,  0,  0},
+	{2,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  3, -2,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  0,  0,  0,  1,  2, -1,  0},
+	{8, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{8, 14,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  2,  0,  0,  0,  0,  0,  0,  0,  0},
+	{3,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  2,  0, -2,  0,  0,  0,  0,  0,  0,  0},
+	{3, -3,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  2, -2,  0,  0,  0,  0,  0,  0,  0,  0},
+	{0,  0,  0,  0,  0,  0,  0,  1, -2,  0,  0}
+};
+
+// Table 23.A; X' columns (p. 154-155)
+static const struct XYZ x_coefficients[TERMS] =
+{
+      //      X' sin   ,     X'  cos
+	{-1719914,   -2,     -25,     0},
+	{    6434,  141,   28007,  -107},
+	{     715,    0,       0,     0},
+	{     715,    0,       0,     0},
+	{     486,   -5,    -236,    -4},
+	{     159,    0,       0,     0},
+	{       0,    0,       0,     0},
+	{      39,    0,       0,     0},
+	{      33,    0,     -10,     0},
+	{      31,    0,       1,     0},
+	{       8,    0,     -28,     0},
+	{       8,    0,     -28,     0},
+	{      21,    0,       0,     0},
+	{     -19,    0,       0,     0},
+	{      17,    0,       0,     0},
+	{      16,    0,       0,     0},
+	{      16,    0,       0,     0},
+	{      11,    0,      -1,     0},
+	{       0,    0,     -11,     0},
+	{     -11,    0,      -2,     0},
+	{      -7,    0,      -8,     0},
+	{     -10,    0,       0,     0},
+	{      -9,    0,       0,     0},
+	{      -9,    0,       0,     0},
+	{       0,    0,      -9,     0},
+	{       0,    0,      -9,     0},
+	{       8,    0,       0,     0},
+	{       8,    0,       0,     0},
+	{      -4,    0,      -7,     0},
+	{      -4,    0,      -7,     0},
+	{      -6,    0,      -5,     0},
+	{      -1,    0,      -1,     0},
+	{       4,    0,      -6,     0},
+	{       0,    0,      -7,     0},
+	{       5,    0,      -5,     0},
+	{       5,    0,       0,     0}
+};
+
+// Table 23.A; Y' columns (p. 154-155)
+static const struct XYZ y_coefficients[TERMS] =
+{
+	{      25,  -13, 1578089,   156},
+	{   25697,  -95,   -5904,  -130},
+	{       6,    0,    -657,     0},
+	{       0,    0,    -656,     0},
+	{    -216,   -4,    -446,     5},
+	{       2,    0,    -147,     0},
+	{       0,    0,      26,     0},
+	{       0,    0,     -36,     0},
+	{      -9,    0,     -30,     0},
+	{       1,    0,     -28,     0},
+	{      25,    0,       8,     0},
+	{     -25,    0,      -8,     0},
+	{       0,    0,     -19,     0},
+	{       0,    0,      17,     0},
+	{       0,    0,     -16,     0},
+	{       0,    0,      15,     0},
+	{       1,    0,     -15,     0},
+	{      -1,    0,     -10,     0},
+	{     -10,    0,       0,     0},
+	{      -2,    0,       9,     0},
+	{      -8,    0,       6,     0},
+	{       0,    0,       9,     0},
+	{       0,    0,      -9,     0},
+	{       0,    0,      -8,     0},
+	{      -8,    0,       0,     0},
+	{       8,    0,       0,     0},
+	{       0,    0,      -8,     0},
+	{       0,    0,      -7,     0},
+	{      -6,    0,      -4,     0},
+	{       6,    0,      -4,     0},
+	{      -4,    0,       5,     0},
+	{      -2,    0,      -7,     0},
+	{      -5,    0,      -4,     0},
+	{      -6,    0,       0,     0},
+	{      -4,    0,      -5,     0},
+	{       0,    0,      -5,     0}
+};
+
+// Table 23.A; Z' columns (p. 154-155)
+static const struct XYZ z_coefficients[TERMS] =
+{
+	{      10,   32,  684185,  -358},
+	{   11141,  -48,   -2559,   -55},
+	{     -15,    0,    -282,     0},
+	{       0,    0,    -285,     0},
+	{     -94,    0,    -193,     0},
+	{      -6,    0,     -61,     0},
+	{       0,    0,      59,     0},
+	{       0,    0,      16,     0},
+	{      -5,    0,     -13,     0},
+	{       0,    0,     -12,     0},
+	{      11,    0,       3,     0},
+	{     -11,    0,      -3,     0},
+	{       0,    0,      -8,     0},
+	{       0,    0,       8,     0},
+	{       0,    0,      -7,     0},
+	{       1,    0,       7,     0},
+	{      -3,    0,      -6,     0},
+	{      -1,    0,       5,     0},
+	{      -4,    0,       0,     0},
+	{      -1,    0,       4,     0},
+	{      -3,    0,       3,     0},
+	{       0,    0,       4,     0},
+	{       0,    0,      -4,     0},
+	{       0,    0,      -4,     0},
+	{      -3,    0,       0,     0},
+	{       3,    0,       0,     0},
+	{       0,    0,      -3,     0},
+	{       0,    0,      -3,     0},
+	{      -3,    0,       2,     0},
+	{       3,    0,      -2,     0},
+	{      -2,    0,       2,     0},
+	{       1,    0,      -4,     0},
+	{      -2,    0,      -2,     0},
+	{      -3,    0,       0,     0},
+	{      -2,    0,      -2,     0},
+	{       0,    0,      -2,     0}
+};
+
+void getAberration(const double JDE, double RA, double Dec, double *correctedRA, double *correctedDec)
+{
+	double A;
+	int i;
+	// speed of light in 10-8 au per day
+	double c = 17314463350.0;
+
+	// calculate T
+	double T = (JDE - 2451545.0) / 36525.0;
+
+	// calculate planetary perturbutions
+	double L2 = 3.1761467 + 1021.3285546 * T;
+	double L3 = 1.7534703 + 628.3075849 * T;
+	double L4 = 6.2034809 + 334.0612431 * T;
+	double L5 = 0.5995464 + 52.9690965 * T;
+	double L6 = 0.8740168 + 21.329909095 * T;
+	double L7 = 5.4812939 + 7.4781599 * T;
+	double L8 = 5.3118863 + 3.8133036 * T;
+	double LL = 3.8103444 + 8399.6847337 * T;
+	double D = 5.1984667 + 7771.3771486 * T;
+	double MM = 2.3555559 + 8328.6914289 * T;
+	double F = 1.6279052 + 8433.4661601 * T;
+
+	double X = 0;
+	double Y = 0;
+	double Z = 0;
+
+	// sum the terms
+	for (i=0; i<TERMS; i++)
+	{
+		A = arguments[i].L2 * L2 + arguments[i].L3 * L3 + arguments[i].L4 * L4 + arguments[i].L5 * L5 +
+		    arguments[i].L6 * L6 + arguments[i].L7 * L7 + arguments[i].L8 * L8 + arguments[i].LL * LL +
+		    arguments[i].D * D   + arguments[i].MM * MM + arguments[i].F * F;
+
+		X += (x_coefficients[i].sin1 + x_coefficients[i].sin2 * T) * sin (A) + (x_coefficients[i].cos1 + x_coefficients[i].cos2 * T) * cos (A);
+		Y += (y_coefficients[i].sin1 + y_coefficients[i].sin2 * T) * sin (A) + (y_coefficients[i].cos1 + y_coefficients[i].cos2 * T) * cos (A);
+		Z += (z_coefficients[i].sin1 + z_coefficients[i].sin2 * T) * sin (A) + (z_coefficients[i].cos1 + z_coefficients[i].cos2 * T) * cos (A);
+	}
+
+	*correctedRA  =  RA + (Y * cos(RA) - X * sin(RA)) / (c * cos(Dec));
+	*correctedDec = Dec + ((X * cos(RA) + Y * sin(RA)) * sin(Dec) - Z * cos(Dec))/-c;
+}
