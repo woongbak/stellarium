77dc939f6b05b158b3f1df7c8832fd5dc6f0c858
Alexander Wolf <alex.v.wolf@gmail.com>
2017-01-04 20:19:52 +0700

    port bugfix #1389765

--- src/core/modules/Planet.cpp
+++ src/core/modules/Planet.cpp
@@ -484,6 +484,17 @@ QVector<const Planet*> Planet::getCandidatesForShadow() const
 	}
 	if (willCastShadow(this, parent.data()))
 		res.append(parent.data());
+	// Test satellites mutual occultations.
+	if (parent.data() != sun) {
+		foreach (const PlanetP& planet, parent.data()->satellites)
+		{
+			//skip self-shadowing
+			if(planet.data() == this )
+				continue;
+			if (willCastShadow(this, planet.data()))
+				res.append(planet.data());
+		}
+	}
 	
 	return res;
 }
